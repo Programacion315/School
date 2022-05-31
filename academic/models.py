c3bfc0bba2db6bbf297b4d2289a751f990d3b5b9
Programacion <jorgeluishuergo88@gmail.com>
2022-05-31 18:11:11 -0500

    feat: adding Bootstrap to the main page

--- academic/models.py
+++ academic/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Grade(models.Model):
-    code = models.CharField(primary_key=True, max_length=6)
+    id = models.CharField(primary_key=True, max_length=6)
     name = models.CharField(max_length=50)
     credits = models.PositiveIntegerField()
+
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.name, self.credits)
     
--- academic/views.py
+++ academic/views.py
@@ -1,3 +1,10 @@
+import imp
+from sqlite3 import Cursor
 from django.shortcuts import render
+from .models import Grade
 
 # Create your views here.
+
+def home(request):
+    coursesListed = Grade.objects.all()
+    return render(request, "courseManagement.html", {"grades":coursesListed})
