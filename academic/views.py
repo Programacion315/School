5e874e8cd1f52c822c54616dc018f5d6f0270ab8
Programacion <jorgeluishuergo88@gmail.com>
2022-06-02 23:20:57 -0500

    feat: change bootstrap to tailwind

--- academic/views.py
+++ academic/views.py
@@ -1,6 +1,6 @@
 import imp
 from sqlite3 import Cursor
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Grade
 
 # Create your views here.
@@ -8,3 +8,11 @@ from .models import Grade
 def home(request):
     coursesListed = Grade.objects.all()
     return render(request, "courseManagement.html", {"grades":coursesListed})
+
+def registerCourse(request):
+    id = request.POST['txtId']
+    name = request.POST['txtName']
+    credits = request.POST['txtCredits']
+
+    grade = Grade.objects.create(id=id, name=name, credits=credits) 
+    return redirect('/')
